ac3cd85d5f9e3100850e857d1ae58dd408429945
Yoyo2550 <desabahagia24@gmail.com>
2026-05-03 09:51:02 +0700

    Refactor Queue implementation: enhance view, insert, push, and pop functions; update main menu for better user interaction

--- 2_Structur_Data/Linear/Queue/Queue.cpp
+++ 2_Structur_Data/Linear/Queue/Queue.cpp
@@ -8,24 +8,72 @@ struct Node{
     Node *next, *prev;
 }*head, *tail, *curr;
 
+void view(){
+    curr = head;
+    while (curr != NULL){
+        printf("%s\n", curr->name);
+        curr = curr->next;
+    }
+}
+
+Node *insert(char name[]){
+    curr = (Node*) malloc (sizeof(Node));
+
+    strcpy(curr->name, name);
+
+    curr->next = curr->prev = NULL;
+
+    return curr;
+}
+
+void push(char name[]){
+    curr = insert(name);
+    
+    if (head == NULL)
+    {
+        head = tail = curr;
+    }else{
+        tail->next = curr;
+        curr->prev = tail;
+        tail = curr;
+    }
+}
+
+void pop(){
+    if (head == tail){
+        head = tail = NULL;
+    }else{
+        curr = head;
+        head = head->next;
+        free(curr);
+        head->prev = NULL;
+    }
+}
+
 int main(){
     int input;
     char name[50];
     do{
-        printf("==== Data ====");
-        //view();
-        printf("==============");
-        printf("1. Insert");
-        printf("1. Delete");
-        printf("0. Exit");
+        system("cls");
+        printf("==== Data ====\n");
+        view();
+        printf("==============\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("0. Exit\n");
         printf("Input : ");
         scanf("%d", &input);
         switch (input){
         case 1:
             printf("Name : ");
             scanf("%s", name);
+            push(name);
             break;
         
+        case 2:
+            pop();
+            break;
+
         default:
             break;
         }
